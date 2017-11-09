d138156665bab4c536a70da61dff2e3f4a20c3cc
Jose Barrios <jose@cognilab.com>
2017-11-09 03:34:27 -0800

    changed validate method to isValid. Added validationErrors method

--- tests/test.js
+++ tests/test.js
@@ -9,115 +9,115 @@ describe('SchemaORG Class\n', function() {
 
   describe('Methods:', function() {
 
-		describe('#validate', function() {
+		describe('#isValid', function() {
 
 			it('url', function() {
 				let valid = 'https://www.google.com';
-				assert.equal(schema.validate(valid, schema.url), true);
+				assert.equal(schema.isValid(valid, schema.url), true);
 				valid = 'http://35.182.10.112/';
-				assert.equal(schema.validate(valid, schema.url), true);
+				assert.equal(schema.isValid(valid, schema.url), true);
 				valid = 'mailto:some@email.com';
-				assert.equal(schema.validate(valid, schema.url), true);
+				assert.equal(schema.isValid(valid, schema.url), true);
 				//Negative tests
 				let invalid = 'text';
-				assert.equal(schema.validate(invalid, schema.url, false), false);
+				assert.equal(schema.isValid(invalid, schema.url, false), false);
 				invalid = 1;
-				assert.equal(schema.validate(invalid, schema.url, false), false);
+				assert.equal(schema.isValid(invalid, schema.url, false), false);
 				invalid = true;
-				assert.equal(schema.validate(invalid, schema.url, false), false);
-				assert.equal(schema.validate(invalid, schema.url, false), false);
+				assert.equal(schema.isValid(invalid, schema.url, false), false);
+				assert.equal(schema.isValid(invalid, schema.url, false), false);
 				invalid = [];
-				assert.equal(schema.validate(invalid, schema.url, false), false);
+				assert.equal(schema.isValid(invalid, schema.url, false), false);
 				invalid = {};
-				assert.equal(schema.validate(invalid, schema.url, false), false);
+				assert.equal(schema.isValid(invalid, schema.url, false), false);
 			});
 
 
 
 			it('text', function() {
 				let valid = 'Some text';
-				assert.equal(schema.validate(valid, schema.text), true);
+				assert.equal(schema.isValid(valid, schema.text), true);
 				//Negative tests
 				let invalid = 1;
-				assert.equal(schema.validate(invalid, schema.text, false), false);
+				assert.equal(schema.isValid(invalid, schema.text, false), false);
 				invalid = true;
-				assert.equal(schema.validate(invalid, schema.text, false), false);
-				assert.equal(schema.validate(invalid, schema.text, false), false);
+				assert.equal(schema.isValid(invalid, schema.text, false), false);
+				assert.equal(schema.isValid(invalid, schema.text, false), false);
 				invalid = [];
-				assert.equal(schema.validate(invalid, schema.text, false), false);
+				assert.equal(schema.isValid(invalid, schema.text, false), false);
 				invalid = {};
-				assert.equal(schema.validate(invalid, schema.text, false), false);
+				assert.equal(schema.isValid(invalid, schema.text, false), false);
 			});
 
 			it('number', function() {
 				let valid = 1;
-				assert.equal(schema.validate(valid, schema.number), true);
+				assert.equal(schema.isValid(valid, schema.number), true);
 				valid = 1.0;
-				assert.equal(schema.validate(valid, schema.number), true);
+				assert.equal(schema.isValid(valid, schema.number), true);
 				valid = -2.13;
-				assert.equal(schema.validate(valid, schema.number), true);
+				assert.equal(schema.isValid(valid, schema.number), true);
 				//Negative tests
 				let invalid = '1';
-				assert.equal(schema.validate(invalid, schema.number, false), false);
+				assert.equal(schema.isValid(invalid, schema.number, false), false);
 				invalid = true;
-				assert.equal(schema.validate(invalid, schema.number, false), false);
-				assert.equal(schema.validate(invalid, schema.number, false), false);
+				assert.equal(schema.isValid(invalid, schema.number, false), false);
+				assert.equal(schema.isValid(invalid, schema.number, false), false);
 				invalid = [];
-				assert.equal(schema.validate(invalid, schema.number, false), false);
+				assert.equal(schema.isValid(invalid, schema.number, false), false);
 				invalid = {};
-				assert.equal(schema.validate(invalid, schema.number, false), false);
+				assert.equal(schema.isValid(invalid, schema.number, false), false);
 			});
 
 			it('integer', function() {
 				let data = 2;
-				assert.equal(schema.validate(data, schema.integer), true);
+				assert.equal(schema.isValid(data, schema.integer), true);
 				data = 0;
-				assert.equal(schema.validate(data, schema.integer), true);
+				assert.equal(schema.isValid(data, schema.integer), true);
 				data = 1.2;
-				assert.equal(schema.validate(data, schema.integer), false);
+				assert.equal(schema.isValid(data, schema.integer), false);
 			});
 
 			it('time', function() {
 				let data = '13:30:00';
-				assert.equal(schema.validate(data, schema.time), true);
+				assert.equal(schema.isValid(data, schema.time), true);
 				data = Date.now();
-				assert.equal(schema.validate(data, schema.time), false);
+				assert.equal(schema.isValid(data, schema.time), false);
 			});
 
 			it('dateTime', function() {
 				let data = '13:30:00';
-				assert.equal(schema.validate(data, schema.dateTime), false);
+				assert.equal(schema.isValid(data, schema.dateTime), false);
 				data = '2011-10-05T14:48:00Z';
-				assert.equal(schema.validate(data, schema.dateTime), true);
+				assert.equal(schema.isValid(data, schema.dateTime), true);
 			});
 
 			it('duration', function() {
 				let data = '13:30:00';
-				assert.equal(schema.validate(data, schema.duration), false);
+				assert.equal(schema.isValid(data, schema.duration), false);
 				data = 'P3Y6M4DT12H30M5S';
-				assert.equal(schema.validate(data, schema.duration), true);
+				assert.equal(schema.isValid(data, schema.duration), true);
 			});
 
 
 			it('boolean', function() {
 				let valid = true;
-				assert.equal(schema.validate(valid, schema.boolean), true);
+				assert.equal(schema.isValid(valid, schema.boolean), true);
 				valid = false;
-				assert.equal(schema.validate(valid, schema.boolean), true);
+				assert.equal(schema.isValid(valid, schema.boolean), true);
 				//Negative tests
 				let invalid = '1';
-				assert.equal(schema.validate(invalid, schema.boolean, false), false);
+				assert.equal(schema.isValid(invalid, schema.boolean, false), false);
 				invalid = [];
-				assert.equal(schema.validate(invalid, schema.boolean, false), false);
+				assert.equal(schema.isValid(invalid, schema.boolean, false), false);
 				invalid = {};
-				assert.equal(schema.validate(invalid, schema.boolean, false), false);
+				assert.equal(schema.isValid(invalid, schema.boolean, false), false);
 			});
 
 			it('structuredValue', function() {
-				assert.equal(schema.validate({}, schema.structuredValue), true);
-				assert.equal(schema.validate(undefined, schema.structuredValue, false), false);
-				assert.equal(schema.validate(null, schema.structuredValue, false), false);
-				assert.equal(schema.validate([], schema.structuredValue, false), false);
+				assert.equal(schema.isValid({}, schema.structuredValue), true);
+				assert.equal(schema.isValid(undefined, schema.structuredValue, false), false);
+				assert.equal(schema.isValid(null, schema.structuredValue, false), false);
+				assert.equal(schema.isValid([], schema.structuredValue, false), false);
 			});
 
 			it('class', function() {
@@ -125,7 +125,7 @@ describe('SchemaORG Class\n', function() {
 				data.category = "Mamal";
 				data.supersededBy = {};
 				data.supersededBy.category = "Animal";
-				assert.equal(schema.validate(data, schema.class), true);
+				assert.equal(schema.isValid(data, schema.class), true);
 			});
 
 			it('property', function() {
@@ -144,7 +144,7 @@ describe('SchemaORG Class\n', function() {
 				data.supersededBy.supersededBy = {};
 				data.supersededBy.supersededBy.category = "Animal";
 				data.inverseOf = JSON.parse(JSON.stringify(data));
-				assert.equal(schema.validate(data, schema.property), true);
+				assert.equal(schema.isValid(data, schema.property), true);
 			});
 
 			it('enumeration', function() {
@@ -153,7 +153,7 @@ describe('SchemaORG Class\n', function() {
 				data.supersededBy.category = "Mamal";
 				data.supersededBy.supersededBy = {};
 				data.supersededBy.supersededBy.category = "Animal";
-				assert.equal(schema.validate(data, schema.enumeration), true);
+				assert.equal(schema.isValid(data, schema.enumeration), true);
 			});
 
 			it('qualitativeValue', function() {
@@ -166,7 +166,7 @@ describe('SchemaORG Class\n', function() {
 				data.equal.valueReference.value = 'One';
 				data.valueReference = {};
 				data.valueReference.value = 'Uno';
-				assert.equal(schema.validate(data, schema.qualitativeValue), true);
+				assert.equal(schema.isValid(data, schema.qualitativeValue), true);
 			});
 
 			it('quantitativeValue', function() {
@@ -175,7 +175,7 @@ describe('SchemaORG Class\n', function() {
 				data.minValue = 0;
 				data.value = true;
 				data.unitCode = 'XXX';
-				assert.equal(schema.validate(data, schema.quantitativeValue), true);
+				assert.equal(schema.isValid(data, schema.quantitativeValue), true);
 			});
 
 			it('propertyValue', function() {
@@ -184,18 +184,18 @@ describe('SchemaORG Class\n', function() {
 				data.minValue = 0;
 				data.value = true;
 				data.unitCode = 'XXX';
-				assert.equal(schema.validate(data, schema.propertyValue), true);
+				assert.equal(schema.isValid(data, schema.propertyValue), true);
 			});
 
 
 			it('person', function() {
 				let data = {};
-				assert.equal(schema.validate(data, schema.person), true);
+				assert.equal(schema.isValid(data, schema.person), true);
 			});
 
 			it('creativework', function() {
 				let data = {};
-				assert.equal(schema.validate(data, schema.creativeWork), true);
+				assert.equal(schema.isValid(data, schema.creativeWork), true);
 			});
 
 
@@ -204,21 +204,21 @@ describe('SchemaORG Class\n', function() {
 			it('thing', function() {
 				let valid = {};
 				valid.additionalType = "http://google.com";
-				assert.equal(schema.validate(valid, schema.thing), true);
+				assert.equal(schema.isValid(valid, schema.thing), true);
 				valid.alternateName = "alternateName";
-				assert.equal(schema.validate(valid, schema.thing), true);
+				assert.equal(schema.isValid(valid, schema.thing), true);
 				valid.description = "description";
-				assert.equal(schema.validate(valid, schema.thing), true);
+				assert.equal(schema.isValiisValid(valid, schema.thing), true);
 				valid.disambiguatingDescription = "disambiguatingDescription";
-				assert.equal(schema.validate(valid, schema.thing), true);
+				assert.equal(schema.isValid(valid, schema.thing), true);
 				valid.identifier = "identifier";
-				assert.equal(schema.validate(valid, schema.thing), true);
+				assert.equal(schema.isValid(valid, schema.thing), true);
 				valid.image = "http://google.com/someimage.png";
-				assert.equal(schema.validate(valid, schema.thing), true);
+				assert.equal(schema.isValid(valid, schema.thing), true);
 				//Negative tests
 				let invalid = {};
 				invalid.image = 1;
-				assert.equal(schema.validate(invalid, schema.thing, false), false);
+				assert.equal(schema.isValid(invalid, schema.thing, false), false);
 			});
 
     });
